e9a75b462830588608e91dbd13998f6e59397ee0
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-17 19:05:26 -0300

    add weights

---
+++ db/migrate/20200817220146_create_weights.rb
@@ -0,0 +1,12 @@
+class CreateWeights < ActiveRecord::Migration[6.0]
+  def change
+    create_table :weights do |t|
+      t.integer    :value,            null: false
+      t.integer    :unity,            null: false
+      t.date       :registration_date
+      t.references :user,             null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
