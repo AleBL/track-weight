33f15ce142a508f19dfa4dda703c7c8533d5f8e5
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-18 17:29:25 -0300

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :diets,   dependent: :destroy
+  has_many :weights, dependent: :destroy
+
   validates :name,   presence: true
   validates :email,  presence: true
   validates :height, presence: true
+
+  validates_numericality_of :height, greater_than: 0
+
   validates_uniqueness_of :email
 end
