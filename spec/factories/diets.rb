bfb45e3554e6160bdb308d73c51fa31907d871a7
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-18 17:31:44 -0300

    add spec validations diets

--- spec/factories/diets.rb
+++ spec/factories/diets.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :diet do
-    initial_date { Date.today }
-    end_date     { Date.tomorrow }
-    user         { nil }
+    initial_date   { Date.today     }
+    end_date       { Date.tomorrow  }
+    initial_weight { build(:weight) }
+    ideal_weight   { build(:weight) }
+    user
   end
 end
--- spec/models/diet_spec.rb
+++ spec/models/diet_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Diet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:diet) }
+
+  it { is_expected.to validate_presence_of(:initial_date) }
+  it { is_expected.to validate_presence_of(:end_date)     }
+
+  it { is_expected.to belong_to(:user)                               }
+  it { is_expected.to belong_to(:initial_weight).dependent(:destroy) }
+  it { is_expected.to belong_to(:ideal_weight)  .dependent(:destroy) }
+  it { is_expected.to have_many(:meals)         .dependent(:destroy) }
+  
+  it { is_expected.to     allow_value(Date.yesterday).for(:initial_date) }
+  it { is_expected.to     allow_value(Date.today)    .for(:initial_date) }
+  it { is_expected.to_not allow_value(Date.tomorrow) .for(:initial_date) }
+
+  it { is_expected.to_not allow_value(Date.yesterday).for(:end_date) }
+  it { is_expected.to     allow_value(Date.today)    .for(:end_date) }
+  it { is_expected.to     allow_value(Date.tomorrow) .for(:end_date) }
 end
